ce92ec1a8da49048236b96432a1dd90c60375583
Sudhir Yadav <thesudhir@gmail.com>
2020-12-20 20:03:04 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+#endif
+    int T;
+    cin >> T;
+    while(T--){
+        
+    }
+    return 0;
+}
